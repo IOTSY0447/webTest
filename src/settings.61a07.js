4d06a527cc8169d5b9ebd9e599339d279d644bd4
lzl <354244489@qq.com>
2020-11-03 18:10:16 +0800

    1

---
+++ src/settings.61a07.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/GameScene.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"f1413",resources:"999aa",main:"fdc10"}};
